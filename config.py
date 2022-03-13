265195c652c562198ece74903d88145c192eb6d7
Gabriel <gabrielfelipe100x@gmail.com>
2022-03-13 15:45:33 -0300

    ajustes

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ DEBUG = True
 SQLALCHEMY_DATABASE_URI =f"postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{db}"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 JSONIFY_PRETTYPRINT_REGULAR = False
-SECRET_KEY = chave
+SECRET_KEY = "MLoD*jIHJay%TnT*6%3l6El^j*Z^pn"
 JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
 JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
 JWT_TOKEN_LOCATION = ["headers", "cookies"]
