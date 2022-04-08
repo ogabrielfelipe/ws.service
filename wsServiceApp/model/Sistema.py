9cfc760b4791b0e245174c5909c715fe1d52a944
Gabriel Felipe Vieira <gabrielfelipe100x@gmail.com>
2022-04-08 08:42:00 -0300

    ajustes

--- wsServiceApp/model/Sistema.py
+++ wsServiceApp/model/Sistema.py
@@ -4,7 +4,7 @@ from .Usuario import db, ma
 class Sistema(db.Model):
     __tablename__ = 'sistema'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    sigla = db.Column(db.String(6), nullable=False)
+    sigla = db.Column(db.String(10), nullable=False)
     nome = db.Column(db.String(150), nullable=False)
 
     def __init__(self, sigla, nome):
