6923db718b863910398d438273febddce559e7a1
Gabriel Felipe Vieira <gabrielfelipe100x@gmail.com>
2022-04-18 14:26:34 -0300

    Ajustes

--- wsServiceApp/bluePrints/atendimento.py
+++ wsServiceApp/bluePrints/atendimento.py
@@ -5,6 +5,7 @@ from ..controller.AtendimentoController import (
     atualiza_atendimento,
     busca_atendimento,
     busca_atendimentos_por_competencia,
+    busca_atendimentos_personalizada,
     delete_atendimento
 )
 import datetime
@@ -34,11 +35,9 @@ def cad_atendimento():
     if datetime.datetime.now() >= exp-datetime.timedelta(minutes=10):
         access_token = create_access_token(identity=identity, fresh=True)
     response = cadastra_atendimento(usuario=get_jwt_identity())
-    response.headers['token_access'] = access_token
-    response.headers['Access-Control-Expose-Headers'] = 'token_access'
-    return response
-
-
+    response[0].headers['token_access'] = access_token
+    response[0].headers['Access-Control-Expose-Headers'] = 'token_access'
+    return response[0], response[1]
 
 
 @atend.route('/Atendimento/Alterar/<codigo>', methods=['PATCH'])
@@ -53,9 +52,9 @@ def alter_atendimento(codigo):
     if datetime.datetime.now() >= exp-datetime.timedelta(minutes=10):
         access_token = create_access_token(identity=identity, fresh=True)
     response = atualiza_atendimento(codigo)
-    response.headers['token_access'] = access_token
-    response.headers['Access-Control-Expose-Headers'] = 'token_access'
-    return response
+    response[0].headers['token_access'] = access_token
+    response[0].headers['Access-Control-Expose-Headers'] = 'token_access'
+    return response[0], response[1]
 
 
 
@@ -71,16 +70,12 @@ def busc_atendimento(codigo):
     if datetime.datetime.now() >= exp-datetime.timedelta(minutes=10):
         access_token = create_access_token(identity=identity, fresh=True)
     response = busca_atendimento(codigo)
-    response.headers['token_access'] = access_token
-    response.headers['Access-Control-Expose-Headers'] = 'token_access'
-    return response
+    response[0].headers['token_access'] = access_token
+    response[0].headers['Access-Control-Expose-Headers'] = 'token_access'
+    return response[0], response[1]
 
 
-'''
-    Consulta atendimento não finalizada
-    Montar a consulta com inner join, pegando as informações das outras tabelas
-'''
-@atend.route('/Atendimento/BuscaAtendimentos', methods=['POST'])
+@atend.route('/Atendimento/BuscaAtendimentosCompetencia', methods=['POST'])
 @jwt_required(locations=["headers"])
 def busc_atendimentos():
     token_client = get_jwt()
@@ -92,9 +87,26 @@ def busc_atendimentos():
     if datetime.datetime.now() >= exp-datetime.timedelta(minutes=10):
         access_token = create_access_token(identity=identity, fresh=True)
     response = busca_atendimentos_por_competencia(identity)
-    response.headers['token_access'] = access_token
-    response.headers['Access-Control-Expose-Headers'] = 'token_access'
-    return response
+    response[0].headers['token_access'] = access_token
+    response[0].headers['Access-Control-Expose-Headers'] = 'token_access'
+    return response[0], response[1]
+
+
+@atend.route('/Atendimento/BuscaAtendimentosPersonalizada', methods=['POST'])
+@jwt_required(locations=["headers"])
+def busc_atendimentos_personalizada():
+    token_client = get_jwt()
+    exp = datetime.datetime.fromtimestamp(token_client['exp'])
+    
+    identity = get_jwt_identity()
+
+    access_token = ''
+    if datetime.datetime.now() >= exp-datetime.timedelta(minutes=10):
+        access_token = create_access_token(identity=identity, fresh=True)
+    response = busca_atendimentos_personalizada()
+    response[0].headers['token_access'] = access_token
+    response[0].headers['Access-Control-Expose-Headers'] = 'token_access'
+    return response[0], response[1]
 
 
 @atend.route('/Atendimento/Excluir/<codigo>', methods=['DELETE'])
@@ -109,6 +121,6 @@ def excluir_atendimento(codigo):
     if datetime.datetime.now() >= exp-datetime.timedelta(minutes=10):
         access_token = create_access_token(identity=identity, fresh=True)
     response = delete_atendimento(codigo)
-    response.headers['token_access'] = access_token
-    response.headers['Access-Control-Expose-Headers'] = 'token_access'
-    return response
+    response[0].headers['token_access'] = access_token
+    response[0].headers['Access-Control-Expose-Headers'] = 'token_access'
+    return response[0], response[1]
