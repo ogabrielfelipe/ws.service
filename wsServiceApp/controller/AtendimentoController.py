38f12123b785d25867ddc30e2f6a93bf2f053b6b
Gabriel Felipe Vieira <gabrielfelipe100x@gmail.com>
2022-04-18 12:23:59 -0300

    ajuste

--- wsServiceApp/controller/AtendimentoController.py
+++ wsServiceApp/controller/AtendimentoController.py
@@ -19,7 +19,7 @@ def cadastra_atendimento(usuario):
     demanda = resp['demanda']
         
     if not resp['dataE']:
-        dataE = ''
+        dataE = ""
     else:
         try:
             dataE = datetime.strptime(resp['dataE'], '%Y-%m-%d').date()
