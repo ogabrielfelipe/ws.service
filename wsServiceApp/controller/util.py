d0ac7fa126fb2be3d237cadf595d1314458c1024
Gabriel <gabrielfelipe100x@gmail.com>
2022-05-01 15:51:04 -0300

    Ajuste na função de Busca Atendimentos

--- wsServiceApp/controller/util.py
+++ wsServiceApp/controller/util.py
@@ -45,33 +45,59 @@ def convert_pesquisa_consulta(dict):
                 operador = valor[i]
                 if valor[i-1] == 'OR':
                     operador_sql_and = valor[i-1]
-                    if isinstance(valor[i+1], float) or isinstance(valor[i+1], int):
-                        cons_sql.append("{}  {} {} {}".format(operador_sql_and, atributo, operador, valor[i+1]))
+                    if valor[i+1] == 'ENUM':
+                        operador_sql_and = valor[i-1]
+                        if isinstance(valor[i+2], float) or isinstance(valor[i+2], int):
+                            cons_sql.append("{}  {} {} {}".format(operador_sql_and, atributo, operador, valor[i+2]))
+                        else:
+                            cons_sql.append("{}  {} {} '{}'".format(operador_sql_and, atributo, operador, valor[i+2])) 
                     else:
-                        cons_sql.append("{}  UNACCENT(UPPER({})) {} UNACCENT(UPPER('{}'))".format(operador_sql_and, atributo, operador, valor[i+1])) 
+                        if isinstance(valor[i+1], float) or isinstance(valor[i+1], int):
+                            cons_sql.append("{}  {} {} {}".format(operador_sql_and, atributo, operador, valor[i+1]))
+                        else:
+                            cons_sql.append("{}  UNACCENT(UPPER({})) {} UNACCENT(UPPER('{}'))".format(operador_sql_and, atributo, operador, valor[i+1])) 
                         
                 operador_sql_and = valor[i-1] 
                 if valor[i-1] == 'AND':
-                    if isinstance(valor[i+1], float) or isinstance(valor[i+1], int):
-                        cons_sql.append("{} {} {} {}".format(operador_sql_and, atributo, operador, valor[i+1]))
+                    if valor[i+1] == 'ENUM':
+                        if isinstance(valor[i+2], float) or isinstance(valor[i+2], int):
+                            cons_sql.append("{} {} {} {}".format(operador_sql_and, atributo, operador, valor[i+2]))
+                        else:
+                            cons_sql.append("{} {} {} '{}'".format(operador_sql_and, atributo, operador, valor[i+2]))
                     else:
-                        cons_sql.append("{} UNACCENT(UPPER({})) {} UNACCENT(UPPER('{}'))".format(operador_sql_and, atributo, operador, valor[i+1]))
+                        if isinstance(valor[i+1], float) or isinstance(valor[i+1], int):
+                            cons_sql.append("{} {} {} {}".format(operador_sql_and, atributo, operador, valor[i+1]))
+                        else:
+                            cons_sql.append("{} UNACCENT(UPPER({})) {} UNACCENT(UPPER('{}'))".format(operador_sql_and, atributo, operador, valor[i+1]))
 
             elif valor[i] == '!=':
                 operador = valor[i]
                 if valor[i-1] == 'OR':
                     operador_sql_and = valor[i-1]
-                    if isinstance(valor[i+1], float) or isinstance(valor[i+1], int):
-                        cons_sql.append("{}  {} {} {}".format(operador_sql_and, atributo, operador, valor[i+1]))
+                    if valor[i+1] == 'ENUM':
+                        operador_sql_and = valor[i-1]
+                        if isinstance(valor[i+2], float) or isinstance(valor[i+2], int):
+                            cons_sql.append("{}  {} {} {}".format(operador_sql_and, atributo, operador, valor[i+2]))
+                        else:
+                            cons_sql.append("{}  {} {} '{}'".format(operador_sql_and, atributo, operador, valor[i+2])) 
                     else:
-                        cons_sql.append("{}  {} {} '{}'".format(operador_sql_and, atributo, operador, valor[i+1]))
+                        if isinstance(valor[i+1], float) or isinstance(valor[i+1], int):
+                            cons_sql.append("{}  {} {} {}".format(operador_sql_and, atributo, operador, valor[i+1]))
+                        else:
+                            cons_sql.append("{}  {} {} '{}'".format(operador_sql_and, atributo, operador, valor[i+1]))
 
                 operador_sql_and = valor[i-1]
                 if valor[i-1] == 'AND':
-                    if isinstance(valor[i+1], float) or isinstance(valor[i+1], int):
-                        cons_sql.append("{} {} {} {}".format(operador_sql_and, atributo, operador, valor[i+1]))
+                    if valor[i+1] == 'ENUM':
+                        if isinstance(valor[i+2], float) or isinstance(valor[i+2], int):
+                            cons_sql.append("{} {} {} {}".format(operador_sql_and, atributo, operador, valor[i+2]))
+                        else:
+                            cons_sql.append("{} {} {} '{}'".format(operador_sql_and, atributo, operador, valor[i+2]))
                     else:
-                        cons_sql.append("{}  {} {} '{}'".format(operador_sql_and, atributo, operador, valor[i+1]))
+                        if isinstance(valor[i+1], float) or isinstance(valor[i+1], int):
+                            cons_sql.append("{} {} {} {}".format(operador_sql_and, atributo, operador, valor[i+1]))
+                        else:
+                            cons_sql.append("{}  {} {} '{}'".format(operador_sql_and, atributo, operador, valor[i+1]))
     
     if cons_sql:  
         return 'WHERE '+" ".join(cons_sql)[4:]
