012d56272286be8890fd4508bc46c0231af2bebb
Gabriel Felipe Vieira <80602315+ogabrielfelipe@users.noreply.github.com>
2022-11-23 16:21:47 -0300

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ from datetime import timedelta
 from dotenv import load_dotenv
 
 
-PASS_KEY = 'JtWhyqEZ'
+PASS_KEY = ''
 
 
 load_dotenv(".env")
@@ -23,7 +23,7 @@ DEBUG = True
 SQLALCHEMY_DATABASE_URI =f"postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{db}"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 JSONIFY_PRETTYPRINT_REGULAR = False
-SECRET_KEY = "MLoD*jIHJay%TnT*6%3l6El^j*Z^pn"
+SECRET_KEY = "MLoD*jIHJay%TnT*6%3sdfs6El^j*Z^pn"
 JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
 JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
 JWT_TOKEN_LOCATION = ["headers", "cookies"]
